8fd09cf1ff6ffc95965e0fc3124e7347a0ca0009
= <63397031+Lucvds99@users.noreply.github.com>
2023-06-27 23:14:41 +0200

    made application ready for public

--- MyMacroManager_DiscordBot/src/main/java/Connection.java
+++ MyMacroManager_DiscordBot/src/main/java/Connection.java
@@ -3,7 +3,7 @@ import discord4j.core.DiscordClient;
 public class Connection {
     private DiscordClient client;
     public Connection() {
-        client = DiscordClient.create("MTExNzAzMTMxMzExMTc4MTUwNg.GApi9T.1jLQiA_SajMpFaW_jKwR7S2zYntnWAkP2bL5D0");
+        client = DiscordClient.create("Server Token here");
     }
 
     public DiscordClient client(){
