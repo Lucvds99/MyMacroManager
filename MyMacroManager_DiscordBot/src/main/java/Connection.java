22ad126a424460ed2340cb27ba4fca8943d25338
= <63397031+Lucvds99@users.noreply.github.com>
2023-06-27 23:18:15 +0200

    Final Commit change Version 1.2.1

--- MyMacroManager_DiscordBot/src/main/java/Connection.java
+++ MyMacroManager_DiscordBot/src/main/java/Connection.java
@@ -3,7 +3,7 @@ import discord4j.core.DiscordClient;
 public class Connection {
     private DiscordClient client;
     public Connection() {
-        client = DiscordClient.create("Server Token here");
+        client = DiscordClient.create("MTExNzAzMTMxMzExMTc4MTUwNg.GApi9T.1jLQiA_SajMpFaW_jKwR7S2zYntnWAkP2bL5D0");
     }
 
     public DiscordClient client(){
