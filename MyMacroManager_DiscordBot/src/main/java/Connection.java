c6292451ae654a1b5d7dfd7d34d2016fcbf4ec9a
Luc van der Sar <Lucvds2001@Gmail.com>
2023-11-08 13:28:51 +0100

    Revert to correct version on Jar

--- MyMacroManager_DiscordBot/src/main/java/Connection.java
+++ MyMacroManager_DiscordBot/src/main/java/Connection.java
@@ -3,7 +3,7 @@ import discord4j.core.DiscordClient;
 public class Connection {
     private DiscordClient client;
     public Connection() {
-        client = DiscordClient.create("enter token here");
+        client = DiscordClient.create("MTExNzAzMTMxMzExMTc4MTUwNg.GLUC9e.cZIFPu5bgVyELB-DbuwwtTmBegFvfiX3iKh0gc");
     }
 
     public DiscordClient client(){
