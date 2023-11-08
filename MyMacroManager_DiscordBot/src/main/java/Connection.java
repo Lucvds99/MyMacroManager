1362a4969dc6eb4187fbc79024aa7557b233e19d
Luc van der Sar <Lucvds2001@Gmail.com>
2023-11-08 14:37:33 +0100

    Update Connection.java

--- MyMacroManager_DiscordBot/src/main/java/Connection.java
+++ MyMacroManager_DiscordBot/src/main/java/Connection.java
@@ -3,7 +3,7 @@ import discord4j.core.DiscordClient;
 public class Connection {
     private DiscordClient client;
     public Connection() {
-        client = DiscordClient.create("MTExNzAzMTMxMzExMTc4MTUwNg.G7EC1t.8FnMPYYQiM9UZrzNKA6qUTqAHT2wNqg_h4QlOs");
+        client = DiscordClient.create("Add bot Token");
     }
 
     public DiscordClient client(){
