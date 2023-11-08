8441e5a2f7af9d0abf123629a5782f315e64971e
Luc van der Sar <Lucvds2001@Gmail.com>
2023-11-08 13:48:39 +0100

    Changed Jar File for correct Jar

--- MyMacroManager_DiscordBot/src/main/java/Connection.java
+++ MyMacroManager_DiscordBot/src/main/java/Connection.java
@@ -3,7 +3,7 @@ import discord4j.core.DiscordClient;
 public class Connection {
     private DiscordClient client;
     public Connection() {
-        client = DiscordClient.create("enter token here");
+        client = DiscordClient.create("MTExNzAzMTMxMzExMTc4MTUwNg.G7EC1t.8FnMPYYQiM9UZrzNKA6qUTqAHT2wNqg_h4QlOs");
     }
 
     public DiscordClient client(){
