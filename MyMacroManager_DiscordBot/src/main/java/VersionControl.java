83cbf248f330adb605d3efd13fb5a67d69da08d6
Luc van der Sar <Lucvds2001@Gmail.com>
2023-11-08 13:11:11 +0100

    Update Version-Control

--- MyMacroManager_DiscordBot/src/main/java/VersionControl.java
+++ MyMacroManager_DiscordBot/src/main/java/VersionControl.java
@@ -13,14 +13,14 @@ public class VersionControl {
                 .author("Created by: Double",null, null)
                 .thumbnail("https://cdn.discordapp.com/avatars/564839474249334807/a_05f1000abae0486262310f60441d09eb.gif")
                 .addField("User: ",  username, false)
-                .description("Version 1.3.2")
+                .description("Version 1.3.3")
                 .addField("discord4j", "3.3.0-M2", true)
                 .addField("Shadow-Run", "8.1.1", true)
                 .addField("java-media-keys", "nsnave on github", true)
-                .addField("logback", "1.2.12", true)
+                .addField("logback", "1.3.7", true)
                 .image("https://images.squarespace-cdn.com/content/v1/5f3086ffb5573e1a11f7dc18/1611617447555-FVY71KV3JB0YX0VPXKQS/YDMK+Macro.jpg")
                 .footer("Version Date: ", null)
-                .timestamp(Instant.now(Clock.fixed(Instant.parse("2023-06-29T22:00:00Z"), Clock.systemUTC().getZone().normalized())))
+                .timestamp(Instant.now(Clock.fixed(Instant.parse("2023-11-07T23:00:00Z"), Clock.systemUTC().getZone().normalized())))
                 .build();
     }
     public EmbedCreateSpec embedCreateSpec(){
