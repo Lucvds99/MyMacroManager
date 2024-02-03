f2ab6072b9ae24e053a8634436a9c34d4df5f630
Double <Lucvds2001@Gmail.com>
2024-02-03 21:02:45 +0100

    Licence update and Connection update

--- MyMacroManager_DiscordBot/src/main/java/Connection.java
+++ MyMacroManager_DiscordBot/src/main/java/Connection.java
@@ -1,14 +1,20 @@
 import discord4j.core.DiscordClient;
-
 public class Connection {
-    private DiscordClient client;
-    private String UserId;
+    private final DiscordClient client;
+    private final String UserId;
+    private final String AutoHotKeyLocation;
     public Connection() {
-        client = DiscordClient.create("add Discord Bot Token");
+        //this is ur bots Private key.
+        client = DiscordClient.create("");
+
         //UserId can be found by turning on developer mode In discord, clicking ur own profile and copying ur user ID, there will be a button at the bottom.
         //UserId is not needed, If you don't want to use the function to show you which device is connected there is no need.
         //If you don't want to use this function leave empty.
         UserId = "";
+
+        //This is the location of the AutoHotKey folder that you will find in the repository. it is automatically set to ProgramFiles.
+        //I do recommend you put it there. if you decide to put the files elsewhere change string below.
+        AutoHotKeyLocation = "C:\\Program Files\\AutoHotkey";
     }
 
     public DiscordClient client(){
@@ -17,5 +23,9 @@ public class Connection {
 
     public String UserId(){
         return UserId;
+    }
+
+    public String AutoHotKey(){
+        return AutoHotKeyLocation;
     }
 }
