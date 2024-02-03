6a11db68bf5068f1daf3d5c58126f6e4fbcc4824
Double <Lucvds2001@Gmail.com>
2024-02-03 16:22:29 +0100

    no solution to the problem with the DLL yet.

--- MyMacroManager_DiscordBot/src/main/java/Main.java
+++ MyMacroManager_DiscordBot/src/main/java/Main.java
@@ -37,7 +37,7 @@ public class Main {
     private static DiscordClient client;
 
 
-    public static void main(String[] args) throws AWTException {
+    public static void main(String[] args) {
 
         while (true) {
             try {
